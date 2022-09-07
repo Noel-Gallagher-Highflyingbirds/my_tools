3e5b4ac7c7114bebd2cb54173534a9559a82618a
carlos-lee123 <1253803956@qq.com>
2022-09-07 16:35:44 +0800

    modify

--- pcd/open3d_fpfh_registration/compile.bat
+++ pcd/open3d_fpfh_registration/compile.bat
@@ -4,4 +4,3 @@ cmake -DOpen3D_ROOT=../../../../install/open3d141_r -S . -B ./build
 cmake --build ./build --config Release --target ALL_BUILD
 copy .\build\Release\RegistrationRANSAC141.exe ..\..\bin /y
 pause
-rem rd /s /q .\build
