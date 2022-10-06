2fa1273851f4f16f78f1bfc00725a1ef7653d7de
carlos-lee123 <1253803956@qq.com>
2022-10-06 23:29:54 +0800

    rm pcl_line_fit

--- pcd/pcl_line_fit/compile.bat
+++
@@ -1,6 +0,0 @@
-rem By carlos_lee 20220313
-rem rd /s /q .\build_r
-cmake -DCMAKE_BUILD_TYPE=Release -DPCL_DIR="D:/carlos/graduate_files/lixin/install/PCL 1.10.0/cmake" -S ./ -B ./build
-cmake --build ./build --config Release --target ALL_BUILD
-copy ./build/Release/line_fit.exe ../../bin /y
-pause
