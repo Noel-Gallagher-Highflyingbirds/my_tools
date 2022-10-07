227a32d0b1c99ca3e4361456909b97ef855f8975
carlos-lee123 <1253803956@qq.com>
2022-10-07 09:41:49 +0800

    update demo

--- pcd/compare_r_t/demo.bat
+++ pcd/compare_r_t/demo.bat
@@ -1,6 +1,4 @@
 by lixin 202206
-set PATH=D:\carlos\code\pcl\pcl-pcl-1.10.0\install_110\bin;D:\carlos\code\pcl\pcl-pcl-1.10.0\install_110\3rdParty\FLANN\bin;D:\carlos\code\pcl\pcl-pcl-1.10.0\install_110\3rdParty\VTK\bin;D:\carlos\code\pcl\pcl-pcl-1.10.0\install_110\3rdParty\Qhull\bin;D:\carlos\code\pcl\pcl-pcl-1.10.0\install_110\3rdParty\OpenNI2\Tools;%PATH%
-
 ..\..\compare_r_t.exe
 ..\..\compare_r_t.exe .\matrix_1reg2.txt .\transformation.txt
 
--- pcd/pcl_fpfh_visualize/demo.bat
+++ pcd/pcl_fpfh_visualize/demo.bat
@@ -1,6 +1,4 @@
 by lixin 202206
-set PATH=I:\graduate_files\2019\code_docs\lixin\install\PCL 1.10.0\bin;I:\graduate_files\2019\code_docs\lixin\install\PCL 1.10.0\3rdParty\FLANN\bin;I:\graduate_files\2019\code_docs\lixin\install\PCL 1.10.0\3rdParty\VTK\bin;I:\graduate_files\2019\code_docs\lixin\install\PCL 1.10.0\3rdParty\Qhull\bin;I:\graduate_files\2019\code_docs\lixin\install\PCL 1.10.0\3rdParty\OpenNI2\Tools;%PATH%
-
 cd ..\..\
 .\pcl_fpfh_visualize.exe .\data\bun000_2pcd.pcd 0.005
 
